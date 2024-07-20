47dd65966a2c763070cdd3329b7d180e6ce46b49
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-07-20 23:06:45 +0000

    Install Crispy Forms, Crispy-Bootstrap5 and add Boostrap CSS+js

--- booking_platform/settings.py
+++ booking_platform/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'hello_world',
     'patient',
 ]
@@ -54,6 +56,10 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
