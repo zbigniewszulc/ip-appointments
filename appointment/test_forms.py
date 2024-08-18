3f6a38329899ef2a0a81d7024b268ab5c8b2fee2
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-08-18 17:14:24 +0000

    Test Book Appointment Form - test 'date' and 'time_slot' fields

--- appointment/test_forms.py
+++ appointment/test_forms.py
@@ -38,3 +38,27 @@ class TestBookAppointmentForm(TestCase):
             appointment_form.is_valid(), 
             msg='Service was not provided, but the form is valid'
         )
+
+    def test_date_required(self):
+        """ Test for 'date' field """
+        appointment_form = BookAppointmentForm({
+            'service': self.service.id,
+            'date': '', 
+            'time_slot': '14:00'
+        })
+        self.assertFalse(
+            appointment_form.is_valid(), 
+            msg='Date was not provided, but the form is valid'
+        )
+
+    def test_time_slot_required(self):
+        """ Test for 'time_slot' field """
+        appointment_form = BookAppointmentForm({
+            'service': self.service.id,
+            'date': '2024-08-23', 
+            'time_slot': ''
+        })
+        self.assertFalse(
+            appointment_form.is_valid(), 
+            msg='Time slot was not provided, but the form is valid'
+        )
