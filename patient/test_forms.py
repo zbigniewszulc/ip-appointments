bb839f8ce61f1e133b33991b1c694ee36c89d0a6
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-08-22 19:09:23 +0000

    Test Patient singup form - test for valid/invalid phone number and 'username' field

--- patient/test_forms.py
+++ patient/test_forms.py
@@ -41,3 +41,62 @@ class TestPatientSignupForm(TestCase):
             patient_form.is_valid(), 
             msg='Form is valid despite missing data'
         )
+
+    def test_username_required(self):
+        """Test the 'username' field"""
+        patient_form = PatientSignupForm({
+            'email': 'test@example.com',
+            'username': '',
+            'first_name': 'Mark',
+            'last_name': 'Hennesy',
+            'date_of_birth': '1990-02-01',
+            'address_line_1': 'Apt 1 Pinewoods',
+            'address_line_2': 'Clodnalkin',
+            'address_line_3': '',
+            'phone_number': '0860860860',
+            'password1': 'kjnkjdfnvdf72jd!2',
+            'password2': 'kjnkjdfnvdf72jd!2'
+        })
+        self.assertFalse(
+            patient_form.is_valid(),
+            msg='Username was not provided, but the form is valid'
+        )
+
+    def test_phone_number_validation(self):
+        """Test for valid and invalid phone numbers"""
+
+        # Valid phone number
+        patient_form = PatientSignupForm({
+            'email': 'test@example.com',
+            'username': 'testuser',
+            'first_name': 'Mark',
+            'last_name': 'Hennesy',
+            'date_of_birth': '1990-02-01',
+            'address_line_1': 'Apt 1 Pinewoods',
+            'address_line_2': 'Clondalkin',
+            'address_line_3': '',
+            'phone_number': '0860860860',
+            'password1': 'kjnkjdfnvdf72jd!2',
+            'password2': 'kjnkjdfnvdf72jd!2'
+        })
+        self.assertTrue(
+            patient_form.is_valid(), msg='Valid phone number was not accepted'
+        )
+
+        # Invalid phone number
+        patient_form = PatientSignupForm({
+            'email': 'test@example.com',
+            'username': 'testuser',
+            'first_name': 'Mark',
+            'last_name': 'Hennesy',
+            'date_of_birth': '1990-02-01',
+            'address_line_1': 'Apt 1 Pinewoods',
+            'address_line_2': 'Clondalkin',
+            'address_line_3': '',
+            'phone_number': 'invalidphone',
+            'password1': 'kjnkjdfnvdf72jd!2',
+            'password2': 'kjnkjdfnvdf72jd!2'
+        })
+        self.assertFalse(
+            patient_form.is_valid(), msg='Invalid phone number was accepted'
+        )
