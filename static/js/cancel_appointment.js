0ed8d9dcad7aaccbc2dadade16dead4b0b239e97
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-01-22 07:17:14 +0000

    Addd more details about the apointment itself when Cancel appointment button clicked

--- static/js/cancel_appointment.js
+++ static/js/cancel_appointment.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     cancelButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             var appointmentId = this.getAttribute('data-appointment-id');
+            var appointmentDate = this.getAttribute('data-appointment-date');
+            var appointmentTimeSlot = this.getAttribute('data-time-slot');
             if (appointmentId) {
                 var cancelUrl = this.getAttribute('data-url');
                 document.getElementById('appointmentId').value = appointmentId;
                 document.getElementById('cancelForm').action = cancelUrl;
+                document.getElementById('cancel-appointment').innerText = `Appointment: ${appointmentDate} at ${appointmentTimeSlot}`;
             } else {
                 alert('Unexpected Error: No appointment ID found. Please contact us.');
             }
